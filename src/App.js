63d2fe62ac4dd4ea8b347a7eae4fb9aad5cd619e
Joshua Balao <joshuacbalao@gmail.com>
2020-08-18 17:32:28 -0400

    add files for app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,106 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Header from './components/header/header'
+import CharacterCard from './components/charCard'
+import './index.css'
+import characters from './characters.json'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    characters, 
+    score: 0,
+    topScore: 0  
+  };
+  compenentDidMount(){
+    this.setState({
+      characters: this.shuffleCharacters(this.state.characters)
+    })
+  }
+
+      shuffleCharacters = (data) => {
+        const datacopy = [...data]
+          for (let i = datacopy.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i +1));
+            [datacopy[i], datacopy[j]] = [datacopy[j], datacopy[i]];
+          }
+          return datacopy;
+      };
+
+      counterCheck = (name) => {
+        let correctGuess = false;
+        const {characters} = this.state;
+        const updatedCharacters = characters.map(characters => {
+          const updatedCharacters = {...character}
+            if (updatedCharacter.name === name) {
+              if (updatedCharacters.selected === false) {
+                updatedCharacter.selected = true;
+                correctGuess = true;
+              }
+            }
+            return updatedCharacter;
+        })
+        console.log(updatedCharacter);
+
+        correctGuess ? this.handleCorrect(updatedCharacters): this.handleIncorrect(updatedCharacters)
+
+      }
+
+      handleCorrect = (characters) =>{
+        const {score, topScore} = this.state;
+        console.log({score, topScore});
+
+        const newScore= score + 1;
+        console.log({newScore})
+
+        const newTopScore = Math.max(newScore, topScore);
+        console.log({newTopScore});
+
+        this.setState({
+          score: newScore,
+          topScore: newTopScore,
+          characters: this.shuffleCharacters(characters)
+        })
+      }
+
+
+      handleIncorrect = (characters) => {
+        alert("Game over!")
+        console.log("handleIncorrect");
+
+        this.setState({
+          characters: this.resetCharacters(characters),
+          score: 0
+        })
+      }
+
+      resetCharacters = (characters) => {
+        const resetCharacters = characters.map(character => ({...character, selected: false}))
+      }
+
+      render(){
+
+        return(
+          <div>
+                <Header score={this.state.score} topScore={this.state.topScore} />
+                <div className="container">
+                  <div className="row">
+                    {this.state.character.map((character) =>
+                    <CharacterCard
+                    key ={character.id}
+                    id  ={character.id}
+                    character={character.name}
+                    selected= {character.selected}
+                    imageURL = {character.imageURL}
+                    counterCheck = {this.counterCheck}
+                    />)}
+                  </div>
+                </div>
+          </div>
+      )
+      };
 }
 
-export default App;
+  export default App;
+  
